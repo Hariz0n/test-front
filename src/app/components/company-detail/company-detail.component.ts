046be897da20f5b4a49afd93bf01cd48806f2907
Petrov Danila <dp200308@gmail.com>
2024-01-22 15:03:26 +0500

    feat: add company detail

--- src/app/components/company-detail/company-detail.component.ts
+++ src/app/components/company-detail/company-detail.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { CompanyResponseDto } from '../../dtos/CompanyResponseDto';
+import { CompaniesService } from '../../services/companies.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-company-detail',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './company-detail.component.html',
   styleUrl: './company-detail.component.scss'
 })
-export class CompanyDetailComponent {
+export class CompanyDetailComponent implements OnInit {
+  protected company$: Observable<CompanyResponseDto> | null = null;
 
+  constructor(private activatedRoute: ActivatedRoute, private companiesService: CompaniesService) {}
+
+  ngOnInit(): void {
+    this.company$ = this.companiesService.getCompany(this.activatedRoute.snapshot.paramMap.get('companyId')!) 
+  }
 }
--- src/app/services/companies.service.ts
+++ src/app/services/companies.service.ts
@@ -8,21 +8,31 @@ import { Observable, shareReplay } from 'rxjs';
 })
 export class CompaniesService {
   private companiesCache$: Observable<CompanyResponseDto[]> | null = null;
+  private companyListCache$: Map<string, Observable<CompanyResponseDto>> =
+    new Map();
 
-  getCompanies() {    
-    if (!this.companiesCache$) {     
-      this.companiesCache$ = this.http.get<CompanyResponseDto[]>(
-        'https://random-data-api.com/api/company/random_company?size=100'
-      ).pipe(shareReplay());
+  getCompanies() {
+    if (!this.companiesCache$) {
+      this.companiesCache$ = this.http
+        .get<CompanyResponseDto[]>(
+          'https://random-data-api.com/api/company/random_company?size=100'
+        )
+        .pipe(shareReplay());
     }
 
     return this.companiesCache$;
   }
 
   getCompany(companyId: string) {
-    return this.http.get(
-      `https://random-data-api.com/api/company/random_company?uid=${companyId}`
-    );
+    if (!this.companyListCache$.has(companyId)) {
+      this.companyListCache$.set(
+        companyId,
+        this.http.get<CompanyResponseDto>(
+          `https://random-data-api.com/api/company/random_company?uid=${companyId}`
+        ).pipe(shareReplay())
+      );
+    }
+    return this.companyListCache$.get(companyId)!;
   }
 
   constructor(private http: HttpClient) {}
